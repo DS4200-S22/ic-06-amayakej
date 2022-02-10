16e405d577dc0c750342e5eb0ce7dff369e6b637
Amaya Kejriwal <amaya@Amayas-MacBook-Air.local>
2022-02-10 13:12:26 -0500

    updated all files

--- main.js
+++ main.js
@@ -6,9 +6,26 @@ Modified: 12/06/21
 
 */
 
+console.log("Hello World");
+
+const MyCourse = "DS4200"
+console.log(MyCourse);
+
+
 //Starter code for Button 1 click
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
+
+    let myRand = Math.random()
+
+    let buttonDiv = document.getElementById("random_num")
+
+    buttonDiv.innerHTML = myRand
+
     console.log("Button 1 was clicked"); 
 }
+
+// calling the function
+
+// printButtom1Clicked();
